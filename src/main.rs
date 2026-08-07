103fa64f5a0cca16803b1e7dc793ee4271fcdaba
karoonimbus <mihletshoko@protonmail.com>
2026-08-07 16:55:43 +0200

    Implemented data field for ICMP Packet
    Just need to implement checksum calculation

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use std::io::{self, Error, ErrorKind};
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::env;
 use raw_socket::{Domain, Protocol, RawSocket, Type};
+use chrono::{DateTime, Local};
 
 fn main() {
     // Takes in a url in the form of domain.zone eg. google.com
     let args: Vec<String> = env::args().collect();
     let  identifier:[u8;2] = [0x00,0x01];
     let mut sequence_n: [u8;2] = [0x00,0x01];
+    let payload: Vec<u8> = build_payload(Local::now());
+    let packet: IcmpPacket = build_packet(8, 0, p_checksum, [0x00,0x01], [0x00,0x01], payload);
 
     //Initialise raw socket
     let socket = RawSocket::new(Domain::ipv4(), Type::raw(), Some(Protocol::icmpv4()));
@@ -19,15 +22,29 @@ fn main() {
     }
 }
 
+//Function to build the packet
 fn build_packet (p_type:u8,p_code:u8,p_checksum:u16,p_identifier:[u8;2],p_sequence_n:[u8;2],payload: Vec<u8>) -> IcmpPacket{
     IcmpPacket { p_type, p_code, p_checksum, p_identifier, p_sequence_n, payload }
 }
 
-fn make_echo_req() -> IcmpPacket {
+// Function to make the ping request
+fn _make_echo_req() -> IcmpPacket {
     todo!();
 }
 
+//Function to Calculate Checksum
+fn calc_checksum(){
+    todo!();
+}
 
+//Function to build payload
+
+fn build_payload(current_time:DateTime<Local>) -> Vec<u8> { 
+    let payload_bytes: Vec<u8> = current_time.to_rfc2822().into_bytes();
+    return payload_bytes;
+}
+
+//The ICMP Packet structure
 struct IcmpPacket {
     p_type: u8,
     p_code: u8,
@@ -37,8 +54,7 @@ struct IcmpPacket {
     payload: Vec<u8>,
 }
 
-
-
+//Fnction to resolve hostname to IP
 fn make_dns_request(url_str: &str) -> io::Result<SocketAddr> {
     let url_str: String = format!("{}:80", url_str);
     //stores result from the to_socket_addrs function
